898098562fe949618acbcb80505bf26e13e0fe33
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-04-05 16:15:52 -0300

    🎉 :tada: initial commit 🎉

---
+++ counterComponent.js
@@ -0,0 +1,16 @@
+const RESTART_BTN = "restartBtn"
+const COUNTER_ID = "counter"
+const COUNTER_VALUE = 100
+const INTERVAL_TIME = 10
+
+class CounterComponent {
+    constructor() {
+        this.initialize()
+    }
+
+    initialize() {
+        console.log('Initilized')
+
+        const counterEl = document.getElementById(COUNTER_ID)
+    }
+}
