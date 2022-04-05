520cd816ce1601f8769579f539c16b4d4f17f02a
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-04-05 17:12:45 -0300

    🚀 :rocket: [Add] Counter - scheduleCounterStop

--- counterComponent.js
+++ counterComponent.js
@@ -13,6 +13,10 @@ class CounterComponent {
             set: (currentContext, propertyKey, newValue) => {
                 console.log({ currentContext, propertyKey, newValue })
 
+                if (!currentContext.value) {
+                    currentContext.stop()
+                }
+
                 currentContext[propertyKey] = newValue
 
                 return true
@@ -27,11 +31,21 @@ class CounterComponent {
         return counter
     }
 
-    updateText = ({counterEl, counter}) => () => {
+    updateText = ({ counterEl, counter }) => () => {
         const textIdentifier = '$$contador'
         const defaultText = `Starting with <strong>${textIdentifier}</strong> seconds...`
 
         counterEl.innerHTML = defaultText.replace(textIdentifier, counter.value--)
+    }
+
+    scheduleCounterStop({ counterEl, intervalId }) {
+        return () => {
+            clearInterval(intervalId)
+
+            counterEl.innerHTML = ""
+
+            this.disableBtn()
+        }
     }
 
     initialize() {
@@ -52,5 +66,15 @@ class CounterComponent {
 
         const fn = this.updateText(args)
         const intervalId = setInterval(fn, INTERVAL_TIME)
+
+        {
+            const args = { counterEl, intervalId }
+
+            const disableBtn = () => console.log('Disabled...')
+            
+            const stopCounterFn = this.scheduleCounterStop.apply({ disableBtn }, [args])
+            
+            counter.stop = stopCounterFn
+        }
     }
 }
